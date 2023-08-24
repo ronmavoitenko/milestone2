32b9accea38473ca76b115b3a4174cc6f575234d
ringo1206 <romavoitenko195@gmail.com>
2023-08-24 09:52:41 +0300

    qwe

--- apps/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task, Comment, TimeLog
-
-# Register your models here
-
-admin.site.register(Task)
-admin.site.register(Comment)
-admin.site.register(TimeLog)
