2e3b1bf140029005cda6f239933d0f24c1a91fbf
ringo1206 <romavoitenko195@gmail.com>
2023-09-04 10:50:15 +0300

    Milestone4.2

--- apps/tasks/management/commands/create_random_data.py
+++ apps/tasks/management/commands/create_random_data.py
@@ -14,25 +14,32 @@ class Command(BaseCommand):
 
         users = User.objects.all()
 
+        task_list = []
+        timelog_list = []
         for i in range(25000):
             task_user = random.choice(users)
             task_owner = random.choice(users)
-            task = Task.objects.create(
+            task = Task(
                 title=f"Задача {i}",
                 description=f"Описание для задачи {i}",
                 owner=task_owner,
                 user=task_user
             )
+            task_list.append(task)
             for j in range(2):
                 start_time = timezone.now() - timezone.timedelta(days=random.randint(0, 30))
                 end_time = start_time + timezone.timedelta(hours=random.randint(1, 10))
                 timer_creater = random.choice(users)
-                TimeLog.objects.create(
+                timelog = TimeLog(
                     task=task,
                     start_time=start_time,
                     end_time=end_time,
                     duration=(end_time - start_time).seconds // 60,
                     user=timer_creater
                 )
+                timelog_list.append(timelog)
+
+        Task.objects.bulk_create(task_list)
+        TimeLog.objects.bulk_create(timelog_list)
 
         self.stdout.write(self.style.SUCCESS('Successfully created random tasks and time logs'))
--- apps/tasks/tests.py
+++ apps/tasks/tests.py
@@ -4,16 +4,15 @@ from django.utils import timezone
 from rest_framework.test import APIRequestFactory, force_authenticate
 from rest_framework.test import APITestCase
 from apps.tasks.models import Task, TimeLog
-from apps.tasks.views import TaskViewSet
+from apps.tasks.views import TaskViewSet, CommentViewSet, TimerViewSet
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 
-from apps.tasks.serializers import ShortTaskSerializer, AllCommentSerializer, TaskListSerializer, TaskAssignSerializer, TimeLogSerializer
-
-
-# Create your tests here.
+from apps.tasks.serializers import ShortTaskSerializer, AllCommentSerializer, TaskListSerializer, TaskAssignSerializer, \
+    TimeLogSerializer, CreateCommentSerializer, StopTimeLogSerializer, CreateTimeLogSerializer
 
 
 class TaskViewSetTestCase(APITestCase):
@@ -52,7 +51,7 @@ class TaskViewSetTestCase(APITestCase):
         response = self.view(request)
         self.assertEqual(response.status_code, 400)
 
-    def test_get_serializer_class(self):
+    def test_get_serializer_class_taskviewset(self):
         view = TaskViewSet()
         view.action = 'retrieve'
         serializer_class = view.get_serializer_class()
@@ -152,3 +151,86 @@ class TaskViewSetTestCase(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_get_serializer_class_commentviewset(self):
+        view = CommentViewSet()
+        view.action = 'create'
+        serializer_class = view.get_serializer_class()
+        self.assertEqual(serializer_class, CreateCommentSerializer)
+        view.action = 'list'
+        serializer_class = view.get_serializer_class()
+        self.assertEqual(serializer_class, view.serializer_class)
+
+    def test_create_comments(self):
+        self.task.owner = self.user
+        self.task.save()
+        url = reverse('comments-list')
+        data = {
+            'text': 'Test Comment',
+            'task': self.task.id
+        }
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_serializer_class_timerviewset(self):
+        view = TimerViewSet()
+        view.action = 'create'
+        serializer_class = view.get_serializer_class()
+        self.assertEqual(serializer_class, view.serializer_class)
+        view.action = 'stop'
+        serializer_class = view.get_serializer_class()
+        self.assertEqual(serializer_class, StopTimeLogSerializer)
+        view.action = 'add_time_log_manually'
+        serializer_class = view.get_serializer_class()
+        self.assertEqual(serializer_class, TimeLogSerializer)
+
+    def test_start_and_stop_time_log(self):
+        url = reverse('timer-list')
+        data = {
+            'task': self.task.id
+        }
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        url = reverse('timer-stop')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_validate_task_with_running_timer(self):
+        self.client.force_authenticate(user=self.user)
+        TimeLog.objects.create(task=self.task, end_time=None, user=self.user)
+        data = {
+            "task": self.task.id,
+        }
+        serializer = CreateTimeLogSerializer(data=data)
+        with self.assertRaises(ValidationError) as context:
+            serializer.is_valid(raise_exception=True)
+        self.assertIn("A timer is already running for this task", str(context.exception))
+
+    def test_validate_task_with_no_running_timer(self):
+        self.client.force_authenticate(user=self.user)
+        data = {
+            "task": self.task.id,
+        }
+        serializer = StopTimeLogSerializer(data=data)
+        with self.assertRaises(ValidationError) as context:
+            serializer.is_valid(raise_exception=True)
+        self.assertIn("No running timer found for this task", str(context.exception))
+
+    def test_add_time_log_manually(self):
+        url = reverse('timer-add-time-log-manually')
+        data = {
+            'task': self.task.id,
+            'start_time': "2023-09-01 12:01",
+            'end_time': "2023-09-01 12:11",
+            'duration': 10
+        }
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_time_logged_last_month(self):
+        url = reverse('timer-get-time-logged-last-month')
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
