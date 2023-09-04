4c9685902cd1cd191f9bf20c94cc447dc8502682
ringo1206 <romavoitenko195@gmail.com>
2023-09-04 09:04:55 +0300

    Milestone4.1

--- apps/common/tests.py
+++ apps/common/tests.py
@@ -12,7 +12,6 @@ class TestCommon(TestCase):
 
     def setUp(self) -> None:
         self.client = APIClient()
-        # check data in fixture json file
         self.test_user1 = User.objects.get(email="user1@email.com")
 
     def test_health_view(self):
